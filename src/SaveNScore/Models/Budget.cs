e9059ec11939ed26be7e2245b7a07a69d8679ae4
bmrghb <bmrghb@mail.umkc.edu>
2019-03-31 16:13:59 -0500

    Display Name Changes, Constraints on user entered values.

--- src/SaveNScore/Models/Budget.cs
+++ src/SaveNScore/Models/Budget.cs
@@ -1,18 +1,36 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace SaveNScore.Models
 {
     public class Budget
     {
-
+        [DisplayName("You shouldn't be seeing this.")] // TEST
         public int CustomerID { get; set; }
+
+        [DisplayName("ID")]
         public int BudgetID { get; set; }
+        
+        [DisplayName("Start Amount")]
+        [Range(0, int.MaxValue)]
         public decimal StartAmount { get; set;}
+
+        [Range(0, int.MaxValue)]
+        [DisplayName("Remaining Amount")]
         public decimal RemainingAmount { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayName("Start Date")]
         public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayName("End Date")]
         public DateTime EndDate { get; set;}
     }
 }
